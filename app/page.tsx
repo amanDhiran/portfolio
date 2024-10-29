c765db127ac190763ac0d96b512487807ef0a78c
amanDhiran <amandhiran201@gmail.com>
2024-10-29 10:06:32 +0530

    fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import Image from "next/image";
 import data from "@/data/socials.json";
 import Icon from "@/components/Icon";
 import TechStack from "@/components/TechStack";
 import ProjectCard from "@/components/ProjectCard";
-import projectData from "@/data/projects.json"
+import projectData from "@/data/projects.json";
 
 export default function Home() {
   return (
-    <div className="mt-6 flex flex-col gap-16">
+    <div className="mt-6 flex flex-col gap-12">
       {/* hero */}
       <div className="flex flex-col md:my-5 gap-6">
         <div>
-          <p className="text-[#FFFFE3] text-lg">Hello, I'm Aman</p>
-          <h1 className="font-bold text-3xl md:text-4xl text-[#FFD074]">
+          <p className="text-[#FFFFE3] text-lg">Hello, I&apos;m Aman</p>
+          <h1 className="font-bold text-3xl md:text-5xl text-[#FFD074]">
             Full Stack Developer
           </h1>
           <p className="mt-2 pl-1 text-[#FFFFE3]">// Based in India</p>
@@ -40,13 +39,25 @@ export default function Home() {
       {/* projects */}
       <section>
         <div className="flex justify-between items-center">
-          <h2 className="font-bold text-2xl md:text-3xl text-[#A374FF]">Projects</h2>
+          <h2 className="font-bold text-2xl md:text-3xl text-[#A374FF]">
+            Projects
+          </h2>
           {/* <a href="#" target="_blank" rel="noopener noreferrer">
             View all
           </a> */}
         </div>
         <div className="mt-11 grid md:grid-cols-2 gap-10">
-          {projectData.map((project) => <ProjectCard  name={project.name} thumbnail={project.href} techStack={project.techStack} key={project.name} live={project.live} code={project.code} description={project.description} />)}
+          {projectData.map((project) => (
+            <ProjectCard
+              name={project.name}
+              thumbnail={project.href}
+              techStack={project.techStack}
+              key={project.name}
+              live={project.live}
+              code={project.code}
+              description={project.description}
+            />
+          ))}
         </div>
       </section>
     </div>
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import skills from "@/data/skills.json";
-import Icon from "./Icon";
 import Image from "next/image";
 export default function TechStack() {
   return (
@@ -8,7 +7,7 @@ export default function TechStack() {
       <h2 className="font-bold text-2xl md:text-3xl text-[#A374FF]">My Tech Stack</h2>
       <div className="flex flex-wrap justify-center gap-4">
         {skills.map((item) => (
-          <div className="flex justify-between border px-3 py-2 rounded-xl border-white/10 gap-2 items-center">
+          <div key={item.name} className="flex justify-between border px-3 py-2 rounded-xl border-white/10 gap-2 items-center">
             <Image width={40} height={20} className="w-8 h-8" src={item.icon} alt={item.name} />
             <p className="text-sm text-[#FFFFE3]">{item.name}</p>
           </div>
